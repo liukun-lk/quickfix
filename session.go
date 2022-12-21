99e9ec757a2224dde15fa6a6eda5c52d266f7484
Igor <drevnickiy.io@exmo.com>
2022-12-21 19:26:29 +0300

    add sync.Once to session.stop func

--- session.go
+++ session.go
@@ -35,6 +35,7 @@ type session struct {
 	stateTimer *internal.EventTimer
 	peerTimer  *internal.EventTimer
 	sentReset  bool
+	stopOnce   sync.Once
 
 	targetDefaultApplVerID string
 
@@ -79,7 +80,10 @@ func (s *session) connect(msgIn <-chan fixIn, msgOut chan<- []byte) error {
 type stopReq struct{}
 
 func (s *session) stop() {
-	s.admin <- stopReq{}
+	//stop once
+	s.stopOnce.Do(func() {
+		s.admin <- stopReq{}
+	})
 }
 
 type waitChan <-chan interface{}
