0a3cf3c788e78ffadd9465e77b7020fe9309641d
Michael Ackley <mackley@connamara.com>
2024-09-09 20:33:41 -0500

    Linter fix

--- session.go
+++ session.go
@@ -502,7 +502,7 @@ func (s *session) handleLogon(msg *Message) error {
 
 	// Verify seq num too high but dont check against app implementation since we just did that.
 	// Don't need to double check.
-	if err := s.verifySelect(msg, false, true, false); err != nil {
+	if err := s.verifyIgnoreSeqNumTooHigh(msg); err != nil {
 		return err
 	}
 
@@ -555,7 +555,7 @@ func (s *session) verify(msg *Message) MessageRejectError {
 }
 
 func (s *session) verifyIgnoreSeqNumTooHigh(msg *Message) MessageRejectError {
-	return s.verifySelect(msg, false, true, true)
+	return s.verifySelect(msg, false, true, false)
 }
 
 func (s *session) verifyIgnoreSeqNumTooHighOrLow(msg *Message) MessageRejectError {
